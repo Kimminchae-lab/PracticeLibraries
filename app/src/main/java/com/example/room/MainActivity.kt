711e70fdc0467dcdab1a5785eff0b95a190aa28e
Kimminchae-lab <rlaalsco1022@gmail.com>
2021-07-28 00:02:09 +0900

    DataBinding + Room

--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.example.room.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        binding.resText.text = db.todoDao().getAll().toString()
+
+        binding.addButton.setOnClickListener {
+            db.todoDao().insert(Todo(binding.todoEdit.text.toString()))
+            binding.resText.text = db.todoDao().getAll().toString()
+            binding.todoEdit.text = null
+        }
     }
 }
